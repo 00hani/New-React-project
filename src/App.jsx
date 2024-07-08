a067dc112095a448e7a1cafe52767f60a34f31a2
khinache <khinachehani00@gmail.com>
2024-07-08 02:21:49 +0100

    created new div in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
 import logo from "./assets/logo.svg";
 import "./App.css";
-
+import Replay from "./components/replay";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>React Project</p>
+        <img src={logo} className="App-logo" alt="logo" /> <p>React Project</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/00hani"
           target="_blank"
+          s
           rel="noopener noreferrer"
         >
-          Learn React
+          My GitHub
         </a>
+        <Replay />
       </header>
     </div>
   );
